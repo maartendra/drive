ef172289e466c97f0650ef9c394a7417df46abbb
Nicolas Clerc <kernicpanel@nclerc.fr>
2026-07-07 13:40:26 +0200

    fixup! ♻️(backend) extract role resolution into a permissions backend

--- src/backend/core/permissions/factory.py
+++ src/backend/core/permissions/factory.py
@@ -1,12 +1,18 @@
 """Permissions backend factory."""
 
+from __future__ import annotations
+
 import functools
+from typing import TYPE_CHECKING
 
 from django.conf import settings
 from django.utils.module_loading import import_string
 
+if TYPE_CHECKING:
+    from core.permissions.backends.base import PermissionsBackend
+
 
 @functools.cache
-def get_permissions_backend():
+def get_permissions_backend() -> PermissionsBackend:
     """Get the permissions backend."""
     return import_string(settings.PERMISSIONS_BACKEND)(**settings.PERMISSIONS_BACKEND_PARAMETERS)
